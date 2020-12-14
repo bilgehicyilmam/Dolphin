cfb7793765388f1fbb349d1fa6de5c8e44b75f4d
Hasan Gökçe <kodyap@gmail.com>
2020-12-14 13:25:13 +0300

    external.py for other scripts

--- ontologies/models.py
+++ ontologies/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from djongo import models
 
 class Ontology(models.Model):
   id = models.BigAutoField(primary_key=True) #important
-  label = models.CharField(max_length = 50, verbose_name = "Label")
-  definition = models.CharField(max_length = 5000, verbose_name = "Definition")
+  class_id = models.CharField(max_length = 500, null=True, verbose_name = "Class")
+  parent_id = models.CharField(max_length = 500, null=True, verbose_name = "Subclass of")
+  label = models.CharField(max_length = 500, verbose_name = "Label")
+  definition = models.CharField(max_length = 5000, null=True, verbose_name = "Definition")
+  synonymous = models.JSONField(default=list, null=True)
+  is_a = models.JSONField(default=list, null=True)
+  has_a = models.JSONField(default=list, null=True)
 
   def __str__(self):
     return self.label
--- utils/Bioontology.py
+++ utils/Bioontology.py
@@ -1,4 +1,16 @@
+from external import *
+from ontologies.models import Ontology
 
+new_person = Ontology(
+  class_id="class",
+  parent_id="parent",
+  label = 'Britney',
+  definition ='test definition',
+  synonymous = ["dummy",3,4],
+  is_a = [2,3,4],
+  has_a = [2,3,4])
 
-class Bioontology:
+new_person.save()
+
+class Bioontology
   pass
