46452dcaac2c53718ba971bd8f062f427f9236e2
Hasan Gökçe <kodyap@gmail.com>
2020-12-14 10:54:19 +0300

    module updates

--- ontologies/models.py
+++ ontologies/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class Ontology(models.Model):
+  id = models.BigAutoField(primary_key=True) #important
   label = models.CharField(max_length = 50, verbose_name = "Label")
   definition = models.CharField(max_length = 5000, verbose_name = "Definition")
 
-  # def __str__(self):
-  #   return self.label
+  def __str__(self):
+    return self.label
