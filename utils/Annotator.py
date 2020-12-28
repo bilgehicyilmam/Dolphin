11e5f667bdc038a3e96ed5f21921ccefb7c44cab
Hasan Gökçe <kodyap@gmail.com>
2020-12-28 16:27:47 +0300

    minor updates

--- utils/Annotator.py
+++ utils/Annotator.py
@@ -2,7 +2,8 @@ from external import *
 from articles.models import article
 from ontologies.models import Ontology
 from datetime import datetime
-
+import re
+import uuid
 
 class Annotator:
 
@@ -15,7 +16,6 @@ class Annotator:
     def get_ontologies(self):
         # returns all ontologies from the database
 
-        # Article.objects.filter(label__text_search='Paul Lennon')
         all_entries = Ontology.objects.all()
         return all_entries
 
@@ -37,7 +37,7 @@ class Annotator:
         print(text)
         return text
 
-    def update_abstract(self, text, label):
+    def update_abstract2(self, text, label):
         # annotate all occurences of a label
 
         print('|--> update_abstract')
@@ -47,9 +47,19 @@ class Annotator:
             # while(text.find(label_with_space) != -1):
                 wrapper = self.create_wrapper(label)
                 print(wrapper)
-                text = text.replace(label, wrapper, 1)
+                text = text.replace(label, wrapper)
         
         return text
+    def update_abstract(self, text, label):
+        i = 0
+        while( i <= len(sample)):
+            print('start while')
+            index = sample.find(word, i)
+            if index != -1:
+                i = i + index + len(word)
+                print(index)
+            else:
+                break
     
     def changer(self):
         pass
@@ -57,6 +67,7 @@ class Annotator:
     def search_keyword(self, keywords):
         # blank
 
+        # Article.objects.filter(label__text_search='Paul Lennon')
         #  Entry.objects.filter(body_text__search='cheese')
         pass
 
@@ -72,17 +83,16 @@ class Annotator:
         abstract = text.replace(keyword, wrapper)
         return abstract
 
-    
-
     def create_stamp(self):
         # returns a time stamp with prefix
 
-        now = datetime.now() # 2020-12-28 10:31:41.593198
-        timestamp = datetime.timestamp(now) # 1609140701.593198
-        timestamp = str(timestamp)
-        timestamp = timestamp.replace('.', '') # 1609140701593198
-        prefix = 'covid19_'
-        idstamp = prefix + timestamp # covid19_1609140701593198
+        # now = datetime.now() # 2020-12-28 10:31:41.593198
+        # timestamp = datetime.timestamp(now) # 1609140701.593198
+        # timestamp = str(timestamp)
+        # timestamp = timestamp.replace('.', '') # 1609140701593198
+        prefix = 'covid19-'
+        # idstamp = prefix + timestamp # covid19_1609140701593198
+        idstamp = prefix + str(uuid.uuid4())
         return idstamp
     
     def create_wrapper(self, keyword):
@@ -117,25 +127,33 @@ developed fractional operator of Atangana-Baleanu. We present briefly the analys
 
 annotator = Annotator()
 
-annotator.abstract_miner(sample)
-# wrapper = annotator.create_wrapper(keyword)
-# results = annotator.update_abstract(sample, keyword, wrapper)
-# print(results)
+# annotator.abstract_miner(sample)
 
-# for result in search_result:
-#     print(result.title)
+word = 'endemic'
+i = 0
+while( i <= len(sample)):
+    print('start while')
+    index = sample.find(word, i)
 
-#       method
+    if index != -1:
+        i = i + index + len(word)
+        print(index)
+    else:
+        break
+        
+# i = len(sample)
+# while( i <= len(sample)):
+#     print('start while')
+#     index = sample.find(word, -i)
 
+#     if index != -1:
+#         i = i - index -len(word)
+#         print(index)
+#     else:
+#         print('not found')
+#         break
 
-
-
-# loop ontologies
-    # for each search the keyword in the abstract
-        # if label found
-            # replace text with wrapper
-
-
+# print(sample.find(word, -50))
 
 
 
@@ -143,3 +161,7 @@ annotator.abstract_miner(sample)
 
 
 # search_result = article.objects.filter(abstract__search='basic reproduction number')
+
+"""
+Geriden takip edersek önüne ne eklediğimizin önemi yok.
+"""
