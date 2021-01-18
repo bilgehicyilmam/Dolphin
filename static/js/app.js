593b4d540a7fc074a1658592f291eeab85281dca
Bilge Hicyilmam <bilge.hicyilmam@boun.edu.tr>
2021-01-18 17:28:23 +0300

    Organize chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,14 @@
 
-
-
 window.onload = function (){
 
     const queryString = window.location.search;
-    console.log(queryString);
     const urlParams = new URLSearchParams(queryString);
     const dim = urlParams.get('dim')
-    console.log(dim);
 
     const element = "q_" + dim
     const q_ = document.getElementById(element); // results
     q_.style.display = "block";
-    console.log(dim);
+
 
 
     //
