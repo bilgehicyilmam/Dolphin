df0c68c26de35a85dd606ae42a3c6bf4afd38305
Marcuse <kaksoy1@binghamton.edu>
2021-01-18 14:28:58 +0200

    Article Detail Page Completed

--- articles/urls.py
+++ articles/urls.py
@@ -5,7 +5,7 @@ from .views import home
 urlpatterns = [
 
    path('', views.home, name='home'),
-   path('articles_detail/', views.article_detail, name='articles_detail'),
+   path('articles_detail/<int:pubmed_id>', views.article_detail, name='articles_detail'),
    #path('', views.search, name='search'),
    path('dimsearch/', views.dimensional_search, name='dimensional_search'),
    path('all_articles/', views.all_articles, name='all_articles'),
