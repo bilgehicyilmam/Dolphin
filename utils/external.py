bacdf6e0e67bd7e39cd561b01c8677eeda02fb2e
Hasan Gökçe <kodyap@gmail.com>
2021-01-31 23:51:16 +0300

    #50 File name change for the old implementation of the Annotator class

--- utils/external.py
+++ utils/external.py
@@ -1,6 +1,11 @@
 from os.path import dirname, abspath
 
+# This method is used external python files to be able to work properly with Django
 def __setup_django(root_path, settings):
+    """
+    :param root_path: Root path of the application
+    :param settings: Django settings
+    """
     import os
     import django
     import sys
@@ -11,7 +16,9 @@ def __setup_django(root_path, settings):
 
 # find main folder of python
 PROJECT_PATH = dirname(dirname(abspath(__file__)))
+
 # setting file
 PROJECT_SETTING = "dolphin.settings"
+
 # setup django
 __setup_django(PROJECT_PATH, PROJECT_SETTING)
